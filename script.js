fc2a9093c76d4d316435caf2525dd2b2296b88a2
williamyeny@gmail.com <williamyeny@gmail.com>
2019-03-19 22:27:13 -0400

    flashing red dot

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  noStroke()
-}
-
 var bh = 10
 var bw = 150
 var bm = 50 // bar margin (from sides)
 
 var mox = 0 // mouse offset x
 var moy = 0 // mouse offset y
+
+var circleTimer = 0
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noStroke()
+
+  textSize(50);
+}
 
 function draw() {
   background(0, 100, 200)
@@ -27,7 +31,20 @@ function draw() {
   rect(windowWidth - bm - bw + mox, windowHeight - bm - bh + moy, bw, bh)
   rect(windowWidth - bm - bh + mox, windowHeight - bm - bw + moy, bh, bw)
 
+  // draw rec
+  text("REC", 150 + mox, 128 + moy)
   
+  // draw dot
+  if (circleTimer < 50) {
+    fill(255, 0, 0)
+    circle(bm + 60 + mox, bm + 60 + moy, 20)
+  }
+  if (circleTimer > 100) {
+    circleTimer = 0
+  }
+  circleTimer ++
+
+
 
 }
 
