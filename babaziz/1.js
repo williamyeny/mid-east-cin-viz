f0fc66b36f2cb5759eac15f822c6172e2f4b2fea
williamyeny@gmail.com <williamyeny@gmail.com>
2019-04-02 00:42:47 -0400

    added sand particles moving based on mouse y

--- babaziz/1.js
+++ babaziz/1.js
@@ -1,6 +1,7 @@
 var darkblue
 var lightblue
 var sands = []
+var backgroundY = 0.0
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -21,15 +22,17 @@ function setup() {
 function draw() {
   // gradient background
   for (i = 0; i <= height; i++) {
-    let inter = map(i, 0, mouseY + height/2, 0, 1);
+    let inter = map(i, 0, backgroundY + height/2, 0, 1);
     let c = lerpColor(darkblue, lightblue, inter);
     stroke(c);
     line(0, i, width, i);
   }
+
+  // sands
   stroke(color(168,138,112))
   strokeWeight(5)
   sands.forEach(function(sand) {
-    point(sand.x, sand.y)
+    point(sand.x, sand.y + backgroundY / 5)
     sand.x += noise(sand.x + sand.seed) * 10
     sand.y += noise(sand.seed)
 
@@ -37,6 +40,9 @@ function draw() {
       delete sand
     }
   })
+
+  // smooth transition to mouseY
+  backgroundY += (mouseY - backgroundY) / 30
 }
 
 function windowResized() {
