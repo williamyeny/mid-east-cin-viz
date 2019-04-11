34285ade4fc3103053273c6c9cbeb186c07fe01a
williamyeny@gmail.com <williamyeny@gmail.com>
2019-04-11 01:18:42 -0400

    new sand

--- babaziz/1.js
+++ babaziz/1.js
@@ -11,35 +11,27 @@ function setup() {
   lightblue = color(195,207,221)
 
   setInterval(function() {
-    sands.push({
-      seed: Math.random() * 100,
-      x: -5,
-      y: Math.random() * height
-    })
-  }, 200)
+    sands.push(new Sand())
+  }, 100)
 }
 
 function draw() {
   // gradient background
-  for (i = 0; i <= height; i++) {
-    let inter = map(i, 0, backgroundY + height/2, 0, 1);
-    let c = lerpColor(darkblue, lightblue, inter);
-    stroke(c);
-    line(0, i, width, i);
-  }
+  // for (i = 0; i <= height; i++) {
+  //   let inter = map(i, 0, backgroundY + height/2, 0, 1);
+  //   let c = lerpColor(darkblue, lightblue, inter);
+  //   stroke(c);
+  //   line(0, i, width, i);
+  // }
+  background(darkblue)
 
   // sands
-  stroke(color(168,138,112))
-  strokeWeight(5)
-  sands.forEach(function(sand) {
-    point(sand.x, sand.y + backgroundY / 5)
-    sand.x += noise(sand.x + sand.seed) * 10
-    sand.y += noise(sand.seed)
-
-    if (sand.x > width) {
-      delete sand
-    }
-  })
+  // stroke(color(168,138,112))
+  
+  for (let s of sands) {
+    s.update(frameCount/60); // update snowflake position
+    s.display(); // draw snowflake
+  }
 
   // smooth transition to mouseY
   backgroundY += (mouseY - backgroundY) / 30
@@ -47,4 +39,35 @@ function draw() {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function Sand() {
+  // initialize coordinates
+  this.posY = 0;
+  this.posX = random(-50, 0);
+  this.initialangle = random(0, 2 * PI);
+  this.size = random(3, 7);
+  this.color = lerpColor(color(187, 167, 142), color(236, 219, 178), random())
+
+  this.radius = sqrt(random(pow(height / 2, 2)));
+
+  this.update = function(time) {
+    // x position follows a circle
+    let w = 0.6; // angular speed
+    let angle = w * time + this.initialangle;
+    this.posY = height / 2 + this.radius * cos(angle);
+
+    // different size snowflakes fall at slightly different y speeds
+    this.posX += pow(this.size, 0.5) + 5;
+
+    // delete sand if past end of screen
+    if (this.posX > width) {
+      delete this
+    }
+  };
+
+  this.display = function() {
+    fill(this.color)
+    ellipse(this.posX, this.posY, this.size);
+  };
 }
