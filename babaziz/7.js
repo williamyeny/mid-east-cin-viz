9b8e59f2383bc7474968c0d074c23f914199442a
williamyeny@gmail.com <williamyeny@gmail.com>
2019-04-12 23:48:51 -0400

    added starfield

--- babaziz/7.js
+++ babaziz/7.js
@@ -1,49 +1,69 @@
-var circles = []
-var radius
-var angle
+let stars = [];
+let speed;
+var mx, my
 
 function setup() {
-  createCanvas(windowWidth, windowHeight)
-  radius = max(width, height)/2
-}
+  createCanvas(windowWidth, windowHeight);
+  
+  for(let i = 0; i < 300; i += 1) {
+    stars[i] = new Star();
+  }
 
+  mx = width/2
+  my = height/2
+} // setup
 
-setInterval(function() {
-  spawnCircle()
-}, 200)
+function Star() {
+  this.z = random(width);
+  this.x = random(-width/2, width/2)
+  this.y = random(-height/2, height/2)
+  
+  this.display = function() {
+    noStroke();
+    let sx = map(this.x / this.z, 0, 1, 0, width);
+    let sy = map(this.y / this.z, 0, 1, 0, height);
+    let r = map(this.z, 0, width, 12, 0);
+    ellipse(sx, sy, r, r);
+  } // display
+  
+  this.update = function() {
+    this.z += speed;
+    
+    if(this.z > width) {
+      this.z = 0
+      // if (random()*2 > 1) {
+      //   this.x = random(-width, -width/2);
+      // } else {
+      //   this.x = random(width/2, width)
+      // }
 
-function spawnCircle() {
-  // random angle
-  angle = random(2 * PI)
-  var circleX = cos(angle)*radius + width/2 // use angle to position circles outside of screen
-  var circleY = sin(angle)*radius + height/2
-  circles.push({
-    x: circleX,
-    y: circleY,
-    size: random(10, 40),
-    speed: 10
-  })
-}
+      // if (random()*2 > 1) {
+      //   this.y = random(-height, -height/2);
+      // } else {
+      //   this.y = random(height/2, height)
+      // }
+      this.x = random(-width/2, width/2)
+      this.y = random(-height/2, height/2)
+      // console.log(this.x)
+      // console.log(this.y)
+    }
+  } // update
+} // Star
 
 function draw() {
-  fill(255)
-  background(0)
-
-  circles.forEach(function(c) {
-    var mouseAngle = atan2(mouseY - c.y, mouseX - c.x)
-    c.x += cos(mouseAngle) * c.speed
-    c.y += sin(mouseAngle) * c.speed
-  
-    ellipse(c.x, c.y, c.size)
-
-    if (c.size <= 0) {
-      circles.splice(circles.indexOf(c), 1)
-      console.log("delete")
-    }
-    
-
-  })
-}
+  background(0, 20);
+  if (mouseX != 0 && mouseY != 0) {
+    mx += (mouseX - mx)/50
+    my += (mouseY - my)/50
+  }
+  translate(mx, my);
+  speed = 5
+  fill(255);
+  for(let i = 0; i < stars.length; i += 1) {
+    stars[i].display();
+    stars[i].update();
+  }
+} // draw 
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
