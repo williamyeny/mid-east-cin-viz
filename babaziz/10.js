a4ff6f8ce59ce7623920706ff9adc914115c6150
williamyeny@gmail.com <williamyeny@gmail.com>
2019-04-15 04:54:20 -0400

    it works!

--- babaziz/10.js
+++ babaziz/10.js
@@ -1,5 +1,4 @@
-var straightBalls = []
-var curveBalls = []
+var balls = []
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
@@ -7,46 +6,49 @@ function setup() {
 }
 
 setInterval(function() {
-  straightBalls.push({
+  var nh = random(height)
+  balls.push({
     x: -10,
-    y: random(height),
-    lastX: -20
+    y: nh,
+    mode: 0,
+    offsetX: 0,
+    offsetY: nh
   })
-  strokeWeight(2)
 }, 2000)
 
 function draw() {
+  noStroke()
   fill(255)
   rect(0, 0, mouseX, height)
-  fill(0)
+  fill(0, 50)
   rect(mouseX, 0, width, height)
 
-  straightBalls.forEach(function(b) {
+  // strokeWeight(1)
+  stroke(0)
+  balls.forEach(function(b) {
     if (b.x > width + 10) {
-      straightBalls.splice(straightBalls.indexOf(b), 1)
+      balls.splice(balls.indexOf(b), 1)
     }
-    if (b.x > mouseX && b.lastX <= mouseX) {
-      
-    } 
-    lastX = b.x
-    b.x += 10
-
+    if (b.x > mouseX && b.mode == 0) {
+      b.offsetX = mouseX
+      b.offsetY = b.y
+      b.mode = 1
+    } else if (b.x < mouseX && b.mode == 1) {
+      b.offsetX = 0
+      b.offsetY = 0
+      b.mode = 0
+    }
+    b.lastX = b.x
     if (b.offsetX > 0) {
-      console.log("oof: " + b.offsetX)
-      b.y = 200*sin(b.x/80 + b.offsetX) + b.offsetY
-      b.x += 5
+      b.y = 500*noise(b.x/100) - 500*noise(b.offsetX/100) + b.offsetY
+      b.x += 2
     } else {
       b.x += 10
     }
 
     fill(255)
-    ellipse(b.x, b.y, 10)
+    ellipse(b.x, b.y, 20)
 
-  })
-
-  curveBalls.forEach(function(b) {
-    b.x += 5
-    b.y = 200*sin(b.x/80)
   })
 } 
 
