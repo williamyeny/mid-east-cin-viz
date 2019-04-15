f13733a09983d68438b3c70e91244966e410bf9c
williamyeny@gmail.com <williamyeny@gmail.com>
2019-04-15 06:19:06 -0400

    added stars

--- babaziz/2.js
+++ babaziz/2.js
@@ -1,18 +1,33 @@
 var circleY = 0
 var circleX = 0
 var angle = 0
+var moonDist = 0
+var stars = []
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke()
+  moonDist = height*3/4
+  for (i = 0; i < 300; i++) {
+    stars.push({
+      dist: random(width), // distance to bottom middle of screen
+      angle: random(TWO_PI), // angle relative to bottom middle of screen (used as offset)
+      size: random(2, 6)
+    })
+  }
 }
 
 function draw() {
   // background
-  background(0, 10)
+  background(0, 50)
 
   fill(color(255))
-  ellipse(mouseX, height - Math.pow(Math.pow(height*3/4, 2) - Math.pow(width/2-mouseX, 2), 0.5), 40, 40)
+  angle += (atan2(mouseY - height, mouseX - width/2) - angle)/10
+  ellipse(cos(angle)*moonDist + width/2, sin(angle)*moonDist + height, 40)
+  
+  stars.forEach(function(s) {
+    ellipse(cos(angle/5 + s.angle)*s.dist + width/2, sin(angle/5 + s.angle)*s.dist + height, s.size)
+  })
 }
 
 function windowResized() {
