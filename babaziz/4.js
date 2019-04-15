12cb925aa62f38381c431c38e3241287adb6b0d5
williamyeny@gmail.com <williamyeny@gmail.com>
2019-04-15 11:21:02 -0400

    finished candle

--- babaziz/4.js
+++ babaziz/4.js
@@ -6,7 +6,7 @@ function setup() {
 }
 
 function draw() {
-  background(0)
+  background(map(dist(flameX, flameY, mouseX, mouseY), 0, width, 0, 50))
 
   // flame
   flameX = width-200 + (noise(frameCount/10)-0.5)*5
@@ -32,10 +32,16 @@ function draw() {
   ellipse(flameX, flameY, 30, 30)
   
   strokeWeight(2)
+  var gradientConst = map(dist(flameX, flameY, mouseX, mouseY), 0, width*2, 0.5, 0)
   for (i = height/2+40; i < height; i += 2) {
-    stroke(223,207,99, 255-i*0.4)
+    stroke(223,207,99, 255-i*gradientConst)
     line(width - 220, i, width - 180, i)
   }
+  noStroke()
+
+  // halo
+  fill(255, 255, 0, 30)
+  ellipse(flameX, flameY, dist(flameX, flameY, mouseX, mouseY)*2)
 }
 
 function windowResized() {
